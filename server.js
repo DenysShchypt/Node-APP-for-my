b2618fb8f8b5dd6c54f6a68fae41b38c8e9a2f56
DenysShchypt <denysshchyptsovit@gmail.com>
2023-12-29 21:18:30 +0200

    Add mongoose

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 import app from "./app.js";
-// Запуск сервера
-app.listen(3000,()=>{console.log("Server on port 3000")})
+import mongoose from "mongoose";
+const DB_HOST = "mongodb+srv://Denys:9jnIRUZcC4wZkFNE@cluster0.uuj4fhd.mongodb.net/?retryWrites=true&w=majority"
+
+// Підключення до бази
+mongoose.connect(DB_HOST).then(
+    // Запуск сервера
+    app.listen(3000, () => { console.log("Server on port 3000")})
+).catch(error => {
+    console.log(error.message);
+    // Команда яка закриває запущені процеси
+    process.exit(1)
+})
